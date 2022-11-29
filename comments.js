cdad496050c533bd4a8bd58e33b372f44a176282
William Ye <wlye@csu.fullerton.edu>
2022-11-29 00:02:08 -0800

    added game logic

--- comments.js
+++ comments.js
@@ -1,98 +1,59 @@
-// https://jsonplaceholder.typicode.com/guide/
+const rockButton = document.getElementById("rock")
+const paperButton = document.getElementById("paper")
+const scissorsButton = document.getElementById("scissors")
+rockButton.addEventListener("click", async event => {
 
-async function downloadPosts (page = 1) {
-  const postsURL = `https://jsonplaceholder.typicode.com/posts?_page=${page}`
-  const response = await fetch(postsURL)
-  const articles = await response.json()
-  return articles
-}
+  const randomNumber = Math.floor(Math.random()*3)
+  if (randomNumber == 0) {
+    console.log("Computer Chose Rock - Tie")
+  }
+  else if (randomNumber == 1) {
+    console.log("Computer Chose Paper - Computer Wins")
+  }
+  else if (randomNumber == 2) {
+    console.log("Computer Chose Scissors - Player Wins")
+  }
+})
 
-async function downloadComments (postId) {
-  const commentsURL = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
-  const response = await fetch(commentsURL)
-  const comments = await response.json()
-  return comments
-}
+paperButton.addEventListener("click", async event => {
+  const randomNumber = Math.floor(Math.random()*3)
+  if (randomNumber == 0) {
+    console.log("Computer Chose Rock - Player Wins")
+  }
+  else if (randomNumber == 1) {
+    console.log("Computer Chose Paper - Tie")
+  }
+  else if (randomNumber == 2) {
+    console.log("Computer Chose Scissors - Computer Wins")
+  }
+})
 
-async function getUserName (userId) {
-  const userURL = `https://jsonplaceholder.typicode.com/users/${userId}`
-  const response = await fetch(userURL)
-  const user = await response.json()
-  return user.name
-}
-
-function getArticleId (comments) {
-  const article = comments.previousElementSibling
-  const data = article.dataset
-  return data.postId
-}
-
-const details = document.getElementsByTagName('details')
-for (const detail of details) {
-  detail.addEventListener('toggle', async event => {
-    if (detail.open) {
-      const asides = detail.getElementsByTagName('aside')
-      const commentsWereDownloaded = asides.length > 0
-      if (!commentsWereDownloaded) {
-        const articleId = getArticleId(detail)
-        const comments = await downloadComments(articleId)
-        console.log(comments)
-      }
-    }
-  })
-}
-
-const posts = await downloadPosts()
-const names = await getUserName()
-console.log(posts)
-
-for (let i = 0; i < posts.length; i++) {
-
-  const main = document.querySelector('main')
-
-  const article = document.createElement("article");
-
-  // Set data-post-id attribute to the id field of the post
-  article.setAttribute("data-post-id", posts[i].id)
-  console.log(posts[i].id)
-
-  const h2 = document.createElement("h2");
-  h2.innerHTML = posts[i].title
-  const aside = document.createElement("aside");
-  aside.innerHTML = "by "
-  const span = document.createElement("span");
-  span.innerHTML = await getUserName(posts[i].userId)
-
-  const p = document.createElement("p");
-
-  //REPLACE BR WITH NEWLINE
-  posts[i].body = posts[i].body.replace(/(?:\r\n|\r|\n)/g, "<br>");
-  p.innerHTML = posts[i].body;
-
-  const details = document.createElement("details");
-  const comment = document.querySelectorAll('details');
-    comment.forEach((comment) => {
-  comment.addEventListener('toggle', logItem);
-});
-  const summary = document.createElement("summary");
-  summary.innerHTML = "See what our readers had to say..."
-  const section = document.createElement("section");
-  const header = document.createElement("header");
-  const h3 = document.createElement("h3");
+scissorsButton.addEventListener("click", async event => {
+  const randomNumber = Math.floor(Math.random()*3)
+  if (randomNumber == 0) {
+    console.log("Computer Chose Rock - Computer Wins")
+  }
+  else if (randomNumber == 1) {
+    console.log("Computer Chose Paper - Player Wins")
+  }
+  else if (randomNumber == 2) {
+    console.log("Computer Chose Scissors - Tie")
+  }
+})
 
 
-  main.appendChild(article)
-  article.appendChild(h2)
-  article.appendChild(aside)
-  aside.appendChild(span)
-  article.appendChild(p)
-  main.appendChild(details)
-  details.appendChild(summary)
-  details.appendChild(section)
-  section.appendChild(header)
-  header.appendChild(h3)
 
-}
-function logItem(e) {
-  console.log('test')
-}
+
+// for (const detail of details) {
+//   detail.addEventListener('toggle', async event => {
+//     if (detail.open) {
+//       const asides = detail.getElementsByTagName('aside')
+//       const commentsWereDownloaded = asides.length > 0
+//       if (!commentsWereDownloaded) {
+//         const articleId = getArticleId(detail)
+//         const comments = await downloadComments(articleId)
+//         console.log(comments)
+//       }
+//     }
+//   })
+// }
