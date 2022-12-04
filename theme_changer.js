82ac56f31ff85e0bd3220fca9620efb0a4d5af66
William Ye <wlye@csu.fullerton.edu>
2022-12-03 18:52:18 -0800

    dark mode stored to local storage

--- theme_changer.js
+++ theme_changer.js
@@ -3,9 +3,28 @@ var background = document.getElementById("base");
 const toggleTheme = function () {
     checkbox.checked ? background.setAttribute("data-theme", "forest")
     : background.setAttribute("data-theme", "lofi")
-       
-    
-   
+    if (checkbox.checked) {
+        localStorage.setItem('darkModeStorage', "on")
     }
-toggleTheme();
+    else {
+        localStorage.setItem('darkModeStorage', "off")
+    }
+}
+//toggleTheme();
 checkbox.addEventListener("click", toggleTheme)
+
+if (localStorage.getItem('darkModeStorage') === null) {
+    console.log("Dark Mode is null")
+    localStorage.setItem('darkModeStorage', "off")
+  } 
+  else {
+      const darkModeStorage = localStorage.getItem('darkModeStorage')
+      if (darkModeStorage === "on") {
+          background.setAttribute("data-theme", "forest")
+          checkbox.checked = true
+      }
+      else {
+          background.setAttribute("data-theme", "lofi")
+      }
+      console.log("test")
+  }
